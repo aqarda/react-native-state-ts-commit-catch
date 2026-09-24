bba204e364ee624d6d76b28159092f8d37ba2b5b
Jabinunger <Jabin.unger@gmail.com>
2026-09-24 09:42:16 -0600

    TodoList.tsx

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,23 +1,37 @@
+import React from "React";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { TodoItem } from "./TodoItem";
 import { colors } from "../assets/theme";
 
-export function TodoList({ todos, onDeleteItem }) {
+export interface Todo {
+id: String;
+title: String;
+description: String;
+}
+
+export interface TodoListProps {
+todos: Todo[];
+onDeleteItem: (id: string) => void;
+
+
+}
+
+export function TodoList({ todos, onDeleteItem }: TodoListProps): React.JSX.Element {
   return (
     <View style={styles.todoListContainer}>
       <Text style={styles.todoSectionTitle}>Your Todos</Text>
       <FlatList
         alwaysBounceVertical={false}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item: Todo): string => item.id}
         data={todos}
-        renderItem={(itemData) => {
+        renderItem={(itemData: ListRenderItemInfo<Todo>): React.JSX.Element => {
           return (
-            // TODO: Update component props
+            // TODO: Update component props :check:
             <TodoItem
               id={itemData.item.id}
               title={itemData.item.title}
               description={itemData.item.description}
-              onDeleteItem={onDeleteItem}
+              onDeleteItem={() => onDeleteItem(itemData.item.id)}
             />
           );
         }}
