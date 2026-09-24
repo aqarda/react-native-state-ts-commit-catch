7a072bf48af924bd6f6a2208b283733e1986f5bb
aqarda <ileandroel14@gmail.com>
2026-09-24 09:34:22 -0600

    Pushing the completed TodoSection in tsx

--- components/TodoSection.tsx
+++ components/TodoSection.tsx
@@ -2,24 +2,27 @@ import { StyleSheet, View } from "react-native";
 import { TodoInput } from "./TodoInput";
 import { TodoList } from "./TodoList";
 import { generateId } from "../utils/generateId";
+import { useState } from "react";
+
+type Todo = {
+  id: string;
+  text: string;
+}
 
 export function TodoSection() {
   // TODO (state): Declare todos as state
-  const todos = [
-    { id: "1", text: "Learn useState" },
-    {
-      id: "2",
-      text: "Practice typescript refactoring",
-    },
-  ];
+const [todos, setTodos] = useState([
+    { id: "1", text: "Learn useState"},
+    { id: "2", text: "Practice typescript refactoring"},
+  ]);
 
-  const addTodoItem = (title) => {
-    const newTodo = { id: generateId(), title };
-    // add item to the existing array
+  const addTodoItem = (text: string): void => {
+    const newTodo = { id: generateId(), text};
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
-  const deleteTodoItem = (id) => {
-    // Delete item state logic
+  const deleteTodoItem = (id: string): void => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
   return (
